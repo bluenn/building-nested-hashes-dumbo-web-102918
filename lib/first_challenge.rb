49b2a71882ad834d4ad21ccf94430ee85b961217
a lan <bluenowte97@gmail.com>
2018-11-08 05:05:16 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,3 +4,10 @@ def first_challenge
      capulet: {}
    }
 end
+#because symbol could write with colon or like this below
+def first_challenge
+   epic_tragedy = {
+     :montague => {},
+     :capulet => {}
+   }
+end
